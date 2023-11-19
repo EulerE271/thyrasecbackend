fe3fa1912fbeb8d142e252b32a091c9b24f0fbe7
Wille <willem.ahlbom@gmail.com>
2023-11-19 17:37:05 +0100

    added .env

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+# Start from a base image with Go installed
+FROM golang:1.21.2 as builder
+
+# Set the working directory inside the container
+WORKDIR /app
+
+# Copy go.mod and go.sum and download the dependencies
+COPY go.mod go.sum ./
+RUN go mod download
+
+# Copy the entire project
+COPY . .
+
+# Change to the directory containing main.go
+WORKDIR /app/cmd/webserver
+
+# Build the application
+RUN CGO_ENABLED=0 GOOS=linux go build -o main .
+
+# Start a new stage from scratch
+FROM alpine:latest  
+WORKDIR /root/
+
+# Copy the binary from the builder stage
+COPY --from=builder /app/cmd/webserver/main .
+
+# Command to run the executable
+CMD ["./main"]
--- internal/common/db/db.go
+++ internal/common/db/db.go
@@ -1,15 +1,31 @@
 package db
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/jmoiron/sqlx"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq" // Import the PostgreSQL driver
 )
 
 var db *sqlx.DB
 
 func Initialize() error {
-	connectionStr := "user=postgres dbname=thyrasec password=root host=localhost sslmode=disable"
-	var err error
+
+	err := godotenv.Load("../../.env") // This goes two levels up from /cmd/webserver
+	if err != nil {
+		return fmt.Errorf("Error loading .env file")
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	dbName := os.Getenv("DB_NAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbSSLMode := os.Getenv("DB_SSLMODE")
+
+	connectionStr := fmt.Sprintf("user=%s dbname=%s password=%s host=%s sslmode=%s", dbUser, dbName, dbPassword, dbHost, dbSSLMode)
+
 	db, err = sqlx.Connect("postgres", connectionStr)
 	if err != nil {
 		return err
