e7a6af6618112499b4b00f5d485f65f547cef885
Wille <willem.ahlbom@gmail.com>
2023-11-23 15:00:05 +0100

    exposed user creation path for dev purposes

--- internal/users/routes/routes.go
+++ internal/users/routes/routes.go
@@ -10,13 +10,12 @@ import (
 func SetupRoutes(router *gin.Engine) {
 	// Public route
 	router.POST("/login", handlers.LoginHandler) // This route is public and outside the protected group
-
+	router.POST("/register/admin", handlers.RegisterAdminHandler)
 	// Group for version 1 APIs with Token Middleware
 	v1 := router.Group("/v1")
 	v1.Use(middleware.TokenMiddleware) // Apply token middleware to all routes in this group
 
 	// Protected routes
-	v1.POST("/register/admin", handlers.RegisterAdminHandler)
 	v1.POST("/register/partner", handlers.RegisterPartnerAdvisorHandler)
 	v1.POST("/register/customer", handlers.RegisterCustomerHandler)
 	v1.GET("/fetch/users", handlers.GetAllUsersHandler)
